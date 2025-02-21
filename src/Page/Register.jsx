4dc6acdeacd683e45b882a81ad8ce475e24ded52
sumonDev9 <sumonmit678@gmail.com>
2025-02-22 02:00:25 +0530

    Update Google Sign-In button text with responsive visibility

--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -25,13 +25,13 @@ const Register = () => {
     
       }
     return (
-        <div className='flex flex-col  justify-center items-center h-screen'>
-            <h1 className='text-xl md:text-3xl font-bold text-[#111111]'>Sign in with Google to Manage Your Tasks</h1>
-            <p className='text-2xl mt-2 text-[#434343]'>Drag, drop, and manage tasks easily.</p>
+        <div className='flex flex-col  justify-center items-center h-screen px-4 md:px-0'>
+            <h1 className='text-lg md:text-2xl lg:3xl font-bold text-[#111111] '>Sign in with Google to Manage Your Tasks</h1>
+            <p className='text-base text-center md:text-xl lg:text-2xl mt-2 text-[#434343]'>Drag, drop, and manage tasks easily.</p>
             <div className='mt-5'>
                 <button onClick={handleGoogleSignIn} class="w-full bg-indigo-500 flex items-center cursor-pointer justify-center gap-2 px-2 py-2 rounded-md">
                     <img className='w-10 cursor-pointer rounded-md bg-white' src="https://i.ibb.co/TcB5YZK/icons8-google-48.png" alt="" />
-                    <span className='text-white text-lg'>Continue with your Goolge task account</span>
+                    <p className='text-white text-lg'><span className="md:hidden">Continue with Goolge</span><span className="hidden md:block">Continue with your Goolge task account</span></p>
                 </button>
             </div>
         </div>
